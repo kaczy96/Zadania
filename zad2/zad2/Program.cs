e0da5eb2a62c1a4e1f3c9536f2b1c26a456f5146
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2018-09-30 17:24:15 +0200

    Zad2
    Added zad2

---
+++ zad2/zad2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Wprowadź wartość z jakiej chcesz obliczyć silnię: ");
+            line:
+            try
+            {
+                int silnia = Int32.Parse(Console.ReadLine());
+                
+                if (silnia == 0)
+                    Console.WriteLine("Wynik: 1");
+                else
+                {
+                    //sposób iteracyjny
+                    int wynikIteracja = 1;
+                    for (int i = 1; i <= silnia; i++)
+                    {
+                        wynikIteracja *= i;
+                    }
+
+                    //sposób rekurencyjny
+                    Silnia silniaRek = new Silnia();
+                    int wynikRekurencja = silniaRek.silniaRekurencyjnie(silnia);
+
+                    Console.WriteLine("Wynik: {0}", wynikIteracja);
+                    Console.WriteLine("Wynik: {0}", wynikRekurencja);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Wprowadź poprawną wartość");
+                goto line;
+            }
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zad2/zad2/Silnia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Silnia
+    {
+        public int  silniaRekurencyjnie (int n)
+        {
+            if (n==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * silniaRekurencyjnie(n - 1);
+            }
+        }
+    }
+}
